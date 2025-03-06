4d3acea997cc4dab4bb88434eed843820cdf8b26
Ageeshagee <ageeshagee123@gmail.com>
2025-03-06 12:50:01 +0530

    config added

--- Client/Myapp/vite.config.js
+++ Client/Myapp/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: ".", // Ensure this points to the correct directory
+  build: {
+    outDir: "dist",
+  },
 })
